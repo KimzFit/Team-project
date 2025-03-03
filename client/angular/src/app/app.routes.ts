521e8ad05cb05c5552f21236ba95410d78dcdeab
kimPrompes224 <patcharakim321341@gmail.com>
2025-03-03 23:12:10 +0700

    login system

--- client/angular/src/app/app.routes.ts
+++ client/angular/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { IndexComponent } from './index/index.component';
 import { TableComponent } from './table/table.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   { path: '', component: IndexComponent }, // หน้าแรก
-  { path: 'table', component: TableComponent } // เส้นทางไป TableComponent
+  { path: 'table', component: TableComponent },
+  { path: 'login', component: LoginComponent }, // เส้นทางไป TableComponent
 ];
