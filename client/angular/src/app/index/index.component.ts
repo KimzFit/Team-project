ad24cd4258843665af260fa71d1f3c3c880e4ea5
Apinya <159605386+PrangApinya@users.noreply.github.com>
2025-03-06 21:30:57 +0700

    จบแล้ววัยรุ่น

--- client/angular/src/app/index/index.component.ts
+++ client/angular/src/app/index/index.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
-
+import { StoryComponent } from '../story/story.component';
+import { InfoComponent } from '../info/info.component';
+import { PictureComponent } from '../picture/picture.component';
 
 @Component({
   selector: 'app-index',
-  imports: [NavbarComponent , FooterComponent ],
+  imports: [NavbarComponent , FooterComponent, StoryComponent, InfoComponent, PictureComponent],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css',
 })
-export class IndexComponent {}
+export class IndexComponent {
+  @ViewChild('pictureSection') pictureSection!: ElementRef;
+
+  scrollToPicture() {
+    if (this.pictureSection) {
+      this.pictureSection.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+}
