c09694cf540ac72843595fc4bc3af8f2278e2f3e
kimPrompes224 <patcharakim321341@gmail.com>
2025-03-07 17:17:31 +0700

    Logout alert box

--- client/angular/src/app/navbar/navbar.component.ts
+++ client/angular/src/app/navbar/navbar.component.ts
@@ -2,34 +2,48 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { AuthService } from '../store/auth.service';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
   isLoggedIn = false;
   userEmail: string | null = null;
 
-  constructor(private authService: AuthService , private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   navigateHome() {
     this.router.navigate(['/']);
   }
-  
+
   ngOnInit() {
-    this.authService.loginState$.subscribe(state => {
+    this.authService.loginState$.subscribe((state) => {
       this.isLoggedIn = state.token !== null;
       this.userEmail = state.payload?.email || null;
     });
   }
 
   logout() {
-    this.authService.logout();
-    this.router.navigate(['/'])
-
+    Swal.fire({
+      title: 'คุณแน่ใจหรือไม่?',
+      text: 'คุณต้องการออกจากระบบใช่หรือไม่?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'ออกจากระบบ',
+      cancelButtonText: 'ยกเลิก',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.authService.logout();
+        this.router.navigate(['/']);
+        Swal.fire('ออกจากระบบสำเร็จ!', 'คุณออกจากระบบเรียบร้อยแล้ว', 'success');
+      }
+    });
   }
 }
